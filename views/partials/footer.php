8c904cef5a3f5c02e5692e160bbb8d476b6dc354
joshuanavarrovelasquez-desing <joshuanavarrovelasquez@gmail.com>
2026-09-01 20:54:12 -0500

    integracion  layout base con Bootstrap en header, nav y footer

---
+++ views/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-dark text-white text-center py-3 mt-5">
+    <div class="container">
+        <small>&copy; 2026 banco_navarro - Todos los derechos reservados</small>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ views/partials/nav.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="index.php">Banco ISTO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php?accion=login">Login</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php?accion=retiro">Retiro</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php?accion=listar">Usuarios</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
