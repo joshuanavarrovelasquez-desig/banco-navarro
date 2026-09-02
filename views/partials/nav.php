491c21d028b492be5171cf7f4a8b5299cf302fcb
joshuanavarrovelasquez-desing <joshuanavarrovelasquez@gmail.com>
2026-09-01 21:36:54 -0500

    se agregp el nav.php para que tenga la info sacada y compartirla con Inicio ,,login, retiro etc

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="index.php">Banco ISTO</a>
+    <a class="navbar-brand fw-bold" href="index.php">Banco navarro</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
