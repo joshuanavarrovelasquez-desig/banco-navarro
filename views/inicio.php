154a4743a0fbfc1848fbe2dbc15d4958dd61d0aa
joshuanavarrovelasquez-desing <joshuanavarrovelasquez@gmail.com>
2026-09-02 19:20:46 -0500

    Vuelvo al code original despues de pruebas

--- views/inicio.php
+++ views/inicio.php
@@ -6,13 +6,6 @@
         <h1 class="display-5 text-primary">Bienvenido al Sistema Bancario</h1>
         <p class="lead">Accede a las funciones desde el menú de navegación o mediante los accesos directos.</p>
         <hr class="my-4">
-        
-        <!-- INTEGRACIÓN PROPIA: Botones de acceso rápido para evitar uso de URL manual -->
-        <div class="d-flex gap-2">
-            <a href="index.php?accion=login" class="btn btn-outline-primary">Ir a Login</a>
-            <a href="index.php?accion=retiro" class="btn btn-outline-success">Realizar Retiro</a>
-            <a href="index.php?accion=listar" class="btn btn-outline-secondary">Ver Usuarios</a>
-        </div>
     </div>
 </div>
 
